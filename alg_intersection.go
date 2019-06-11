a83357b3e0afbc24ac948a700c0163f5b1209de0
Peter Stace <peterstace@gmail.com>
2019-06-12 06:21:55 +1000

    Clean up intersection code

--- alg_intersection.go
+++ alg_intersection.go
@@ -10,15 +10,16 @@ func intersectLineWithLine(n1, n2 Line) Geometry {
 	c := n2.a.XY
 	d := n2.b.XY
 
-	e := (c.Y-d.Y)*(a.X-c.X) + (d.X-c.X)*(a.Y-c.Y)
-	f := (d.X-c.X)*(a.Y-b.Y) - (a.X-b.X)*(d.Y-c.Y)
-	g := (a.Y-b.Y)*(a.X-c.X) + (b.X-a.X)*(a.Y-c.Y)
-	h := (d.X-c.X)*(a.Y-b.Y) - (a.X-b.X)*(d.Y-c.Y)
-
-	if f != 0 && h != 0 {
+	if parallel := cross(sub(b, a), sub(d, c)) == 0; !parallel {
+		e := (c.Y-d.Y)*(a.X-c.X) + (d.X-c.X)*(a.Y-c.Y)
+		f := (d.X-c.X)*(a.Y-b.Y) - (a.X-b.X)*(d.Y-c.Y)
+		g := (a.Y-b.Y)*(a.X-c.X) + (b.X-a.X)*(a.Y-c.Y)
+		h := (d.X-c.X)*(a.Y-b.Y) - (a.X-b.X)*(d.Y-c.Y)
+		// Division by zero is not possible, since the lines are not parallel.
 		p := e / f
 		q := g / h
 		if p < 0 || p > 1 || q < 0 || q > 1 {
+			// Intersection between lines occurs beyond line endpoints.
 			return NewGeometryCollection(nil)
 		}
 		pt, err := NewPoint(
@@ -31,9 +32,7 @@ func intersectLineWithLine(n1, n2 Line) Geometry {
 		return pt
 	}
 
-	// They're parallel. But are they colinear?
-	if cross(sub(b, a), sub(d, a)) == 0 {
-		// The two segments are colinear, so the intersection is another line.
+	if colinear := cross(sub(b, a), sub(d, a)) == 0; colinear {
 		u := XY{
 			math.Max(math.Min(a.X, b.X), math.Min(c.X, d.X)),
 			math.Max(math.Min(a.Y, b.Y), math.Min(c.Y, d.Y)),
@@ -43,15 +42,18 @@ func intersectLineWithLine(n1, n2 Line) Geometry {
 			math.Min(math.Max(a.Y, b.Y), math.Max(c.Y, d.Y)),
 		}
 		if u.X > v.X || u.Y > v.Y {
+			// Line segments don't overlap at all.
 			return NewGeometryCollection(nil)
 		}
 		if u == v {
+			// Line segments overlap at a point.
 			pt, err := NewPoint(u.X, u.Y)
 			if err != nil {
 				panic(err)
 			}
 			return pt
 		}
+		// Line segments overlap over a line segment.
 		ln, err := NewLine(Coordinates{u}, Coordinates{v})
 		if err != nil {
 			panic(err)
@@ -59,7 +61,7 @@ func intersectLineWithLine(n1, n2 Line) Geometry {
 		return ln
 	}
 
-	// Parrallel but not colinear. So cannot intersect anywhere.
+	// Parrallel but not colinear, so cannot intersect anywhere.
 	return NewGeometryCollection(nil)
 }
 
