53366ee3d990e8898bad1e2b6179ac8ed63de26c
Peter Stace <peterstace@gmail.com>
2019-06-16 07:40:54 +1000

    Implement Equals for Point/Point

--- alg_equals.go
+++ alg_equals.go
@@ -1,5 +1,11 @@
 package simplefeatures
 
 func equals(g1, g2 Geometry) bool {
+	pt1, ok1 := g1.(Point)
+	pt2, ok2 := g2.(Point)
+	if ok1 && ok2 {
+		return pt1.coords.XY == pt2.coords.XY
+	}
+
 	panic("not implemented")
 }
---
+++ alg_equals_test.go
@@ -0,0 +1,60 @@
+package simplefeatures_test
+
+import (
+	"strings"
+	"testing"
+
+	. "github.com/peterstace/simplefeatures"
+)
+
+func TestNotEquals(t *testing.T) {
+	wkts := []string{
+		"POINT(1 2)",
+		"POINT(2 1)",
+	}
+	for i := range wkts {
+		for j := range wkts {
+			if i == j {
+				continue
+			}
+			t.Run(wkts[i]+"=="+wkts[j], func(t *testing.T) {
+				g1, err := UnmarshalWKT(strings.NewReader(wkts[i]))
+				if err != nil {
+					t.Fatal(err)
+				}
+				g2, err := UnmarshalWKT(strings.NewReader(wkts[j]))
+				if err != nil {
+					t.Fatal(err)
+				}
+				if g1.Equals(g2) {
+					t.Errorf("expected not be be equal")
+				}
+			})
+		}
+	}
+}
+
+func TestEquals(t *testing.T) {
+	for _, equalSet := range [][]string{
+		{"POINT (1 1)"},
+		{"POINT (2 3)"},
+	} {
+		for i := range equalSet {
+			for j := range equalSet {
+				t.Run(equalSet[i]+"=="+equalSet[j], func(t *testing.T) {
+					g1, err := UnmarshalWKT(strings.NewReader(equalSet[i]))
+					if err != nil {
+						t.Fatal(err)
+					}
+					g2, err := UnmarshalWKT(strings.NewReader(equalSet[j]))
+					if err != nil {
+						t.Fatal(err)
+					}
+					if !g1.Equals(g2) {
+						t.Errorf("expected to be equal")
+					}
+				})
+			}
+		}
+	}
+}
