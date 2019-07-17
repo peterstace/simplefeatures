f2ac6561c3bad9b111e90e0653c15ba7dcc3e12f
Peter Stace <peterstace@gmail.com>
2019-07-17 19:42:30 +1000

    Get Point and LineString WKB parsing working

--- wkb_parser.go
+++ wkb_parser.go
@@ -2,8 +2,11 @@ package simplefeatures
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
+	"log"
+	"strconv"
 )
 
 // UnmarshalWKB reads the Well Known Binary (WKB), and returns the
@@ -12,16 +15,32 @@ func UnmarshalWKB(r io.Reader) (Geometry, error) {
 	p := wkbParser{r: r}
 	p.parseByteOrder()
 	p.parseGeomType()
-
-	return nil, p.err
+	geom := p.parseGeomRoot()
+	// TODO: check for trailing bytes
+	return geom, p.err
 }
 
+type coordType byte
+
+const (
+	coordTypeXY   coordType = 1
+	coordTypeXYZ  coordType = 2
+	coordTypeXYM  coordType = 3
+	coordTypeXYZM coordType = 4
+)
+
 type wkbParser struct {
-	r        io.Reader
-	err      error
-	bo       binary.ByteOrder
-	coordLen int // 2 for XY, 3 for XYZ, 3 for XYM, 4 for XYZM
-	geomType uint32
+	err       error
+	r         io.Reader
+	bo        binary.ByteOrder
+	geomType  int
+	coordType coordType
+}
+
+func (p *wkbParser) setErr(err error) {
+	if p.err == nil {
+		p.err = err
+	}
 }
 
 func (p *wkbParser) parseByteOrder() {
@@ -43,21 +62,111 @@ func (p *wkbParser) parseByteOrder() {
 	}
 }
 
+func (p *wkbParser) parseUint32() uint32 {
+	var x uint32
+	p.read(&x)
+	return x
+}
+
 func (p *wkbParser) parseGeomType() {
+	geomCode := p.parseUint32()
+	p.geomType = int(geomCode % 1000)
+	switch geomCode / 1000 {
+	case 0:
+		p.coordType = coordTypeXY
+	case 1:
+		p.coordType = coordTypeXYZ
+	case 2:
+		p.coordType = coordTypeXYM
+	case 3:
+		p.coordType = coordTypeXYZM
+	default:
+		p.setErr(errors.New("cannot determine coordinate type"))
+	}
+}
+
+func (p *wkbParser) parseGeomRoot() Geometry {
+	if p.err != nil {
+		return nil
+	}
+	switch p.geomType {
+	case 1:
+		return NewPointFromCoords(p.parsePoint())
+	case 2:
+		log.Println("case 2")
+		coords := p.parseLineString()
+		switch len(coords) {
+		case 0:
+			return NewEmptyLineString()
+		case 2:
+			ln, err := NewLine(coords[0], coords[1])
+			p.setErr(err)
+			return ln
+		default:
+			ls, err := NewLineString(coords)
+			p.setErr(err)
+			return ls
+		}
+	default:
+		p.setErr(fmt.Errorf("unknown geometry type: %d", p.geomType))
+		return nil
+	}
+}
+
+func (p *wkbParser) read(ptr interface{}) {
 	if p.err != nil {
 		return
 	}
-	if err := binary.Read(p.r, p.bo, &p.geomType); err != nil {
-		p.err = err
-		return
+	p.err = binary.Read(p.r, p.bo, ptr)
+}
+
+func (p *wkbParser) parsePoint() Coordinates {
+	x := p.parseFloat64()
+	y := p.parseFloat64()
+	var z, m float64
+	switch p.coordType {
+	case coordTypeXY:
+	case coordTypeXYZ:
+		z = p.parseFloat64()
+	case coordTypeXYM:
+		m = p.parseFloat64()
+	case coordTypeXYZM:
+		z = p.parseFloat64()
+		m = p.parseFloat64()
+	default:
+		p.err = errors.New("unknown coord type")
+		return Coordinates{}
 	}
+
+	// Only XY is supported so far.
+	_, _ = z, m
+	xs, err := NewScalar(strconv.FormatFloat(x, 'f', -1, 64))
+	if err != nil {
+		p.err = err
+		return Coordinates{}
+	}
+	ys, err := NewScalar(strconv.FormatFloat(y, 'f', -1, 64))
+	if err != nil {
+		p.err = err
+		return Coordinates{}
+	}
+	return Coordinates{XY{xs, ys}}
 }
 
-type wkbGeomType struct {
-	name     string
-	coordLen int
+func (p *wkbParser) parseLineString() []Coordinates {
+	n := p.parseUint32()
+	coords := make([]Coordinates, n)
+	for i := range coords {
+		coords[i] = p.parsePoint()
+	}
+	return coords
 }
 
-var wkbGeomTypes = map[uint32]wkbGeomType{
-	1: wkbGeomType{"Point", 2},
+func (p *wkbParser) parseFloat64() float64 {
+	if p.err != nil {
+		return 0
+	}
+	var f float64
+	p.read(&f)
+	return f
 }
--- wkb_parser_test.go
+++ wkb_parser_test.go
@@ -3,6 +3,7 @@ package simplefeatures_test
 import (
 	"bytes"
 	"strconv"
+	"strings"
 	"testing"
 
 	. "github.com/peterstace/simplefeatures"
@@ -25,17 +26,88 @@ func hexStringToBytes(t *testing.T, s string) []byte {
 }
 
 func TestWKBParser(t *testing.T) {
-	for _, tt := range []struct {
-		wkt string
+	for i, tt := range []struct {
 		wkb string
+		wkt string
 	}{
 		{
+			// POINT(1 2)
+			wkb: "0101000000000000000000f03f0000000000000040",
 			wkt: "POINT(1 2)",
-			wkb: "0101000000000000000000F03F0000000000000040",
+		},
+		{
+			// POINTZ(1 2 3)
+			wkb: "01e9030000000000000000f03f00000000000000400000000000000840",
+			wkt: "POINT(1 2)",
+		},
+		{
+			// POINTM(1 2 3)
+			wkb: "01d1070000000000000000f03f00000000000000400000000000000840",
+			wkt: "POINT(1 2)",
+		},
+		{
+			// POINTZM(1 2 3 4)
+			wkb: "01b90b0000000000000000f03f000000000000004000000000000008400000000000001040",
+			wkt: "POINT(1 2)",
+		},
+		{
+			// LINESTRING EMPTY
+			wkb: "010200000000000000",
+			wkt: "LINESTRING EMPTY",
+		},
+		{
+			// LINESTRINGZ EMPTY
+			wkb: "01ea03000000000000",
+			wkt: "LINESTRING EMPTY",
+		},
+		{
+			// LINESTRINGM EMPTY
+			wkb: "01d207000000000000",
+			wkt: "LINESTRING EMPTY",
+		},
+		{
+			// LINESTRINGZM EMPTY
+			wkb: "01ba0b000000000000",
+			wkt: "LINESTRING EMPTY",
+		},
+		{
+			// LINESTRING(1 2,3 4)
+			wkb: "010200000002000000000000000000f03f000000000000004000000000000008400000000000001040",
+			wkt: "LINESTRING(1 2,3 4)",
+		},
+
+		{
+			// LINESTRINGZ(1 2 3,4 5 6)
+			wkb: "01ea03000002000000000000000000f03f00000000000000400000000000000840000000000000104000000000000014400000000000001840",
+			wkt: "LINESTRING(1 2,4 5)",
+		},
+		{
+			// LINESTRINGM(1 2 3,4 5 6)
+			wkb: "01d207000002000000000000000000f03f00000000000000400000000000000840000000000000104000000000000014400000000000001840",
+			wkt: "LINESTRING(1 2,4 5)",
+		},
+		{
+			// LINESTRINGZM(1 2 3 4,5 6 7 8)
+			wkb: "01ba0b000002000000000000000000f03f000000000000004000000000000008400000000000001040000000000000144000000000000018400000000000001c400000000000002040",
+			wkt: "LINESTRING(1 2,5 6)",
 		},
 	} {
-		_, err := UnmarshalWKB(bytes.NewReader(hexStringToBytes(t, tt.wkb)))
-		expectNoErr(t, err)
-		//expectDeepEqual(t, geom, geomFromWKT(t, tt.wkt))
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			geom, err := UnmarshalWKB(bytes.NewReader(hexStringToBytes(t, tt.wkb)))
+			expectNoErr(t, err)
+			expectDeepEqual(t, geom, geomFromWKT(t, tt.wkt))
+		})
+	}
+}
+
+func TestWKBParserInvalidGeometryType(t *testing.T) {
+	// Same as POINT(1 2), but with the geometry type byte set to 0xff.
+	const wkb = "01ff000000000000000000f03f0000000000000040"
+	_, err := UnmarshalWKB(bytes.NewReader(hexStringToBytes(t, wkb)))
+	if err == nil {
+		t.Errorf("expected an error but got nil")
+	}
+	if !strings.Contains(err.Error(), "unknown geometry type") {
+		t.Errorf("expected to be an error about unknown geometry type, but got: %v", err)
 	}
 }
