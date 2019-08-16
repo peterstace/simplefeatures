eae164cce4fb8fd7a3064f188d1a9903a36cde57
Peter Stace <peterstace@gmail.com>
2019-08-16 17:07:41 +1000

    Refine some comments

--- geom/ctor_options.go
+++ geom/ctor_options.go
@@ -3,12 +3,12 @@ package geom
 // ConstructorOption allows the behaviour of Geometry constructor functions to be modified.
 type ConstructorOption func(o *ctorOptionSet)
 
-// DisableExpensiveValidations disables validations during Geometry
-// construction that are computationally expensive. There are several
-// implications as a result of using this option:
+// DisableExpensiveValidations gives a hint that geometry constructors may opt
+// to skip any expensive validations. There are several implications as a
+// result of using this option:
 //
-// 1. If the geometry is invalid, then the results of geometric calculations
-//    resulting from the geometry may be invalid.
+// 1. If the geometry is invalid, then any geometric calculations resulting
+//    from the geometry may be invalid.
 //
 // 2. If the geometry is invalid, then invoking geometric calculations may
 //    cause a panic or infinite loop (this is a theoretical concern that
