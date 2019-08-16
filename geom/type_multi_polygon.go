96ad6c70068374b384ddad0b8a1cbac53224cb65
Peter Stace <peterstace@gmail.com>
2019-08-16 17:20:39 +1000

    Skip MultiPolygon validation

--- geom/type_multi_polygon.go
+++ geom/type_multi_polygon.go
@@ -23,16 +23,18 @@ type MultiPolygon struct {
 // NewMultiPolygon creates a MultiPolygon from its constintuent Polygons. It
 // gives an error if any of the MultiPolygon assertions are not maintained.
 func NewMultiPolygon(polys []Polygon, opts ...ConstructorOption) (MultiPolygon, error) {
-	for i := 0; i < len(polys); i++ {
-		for j := i + 1; j < len(polys); j++ {
-			bound1 := polys[i].Boundary()
-			bound2 := polys[j].Boundary()
-			inter := bound1.Intersection(bound2)
-			if inter.Dimension() > 0 {
-				return MultiPolygon{}, errors.New("the boundaries of the polygon elements of multipolygons must only intersect at points")
-			}
-			if polyInteriorsIntersect(polys[i], polys[j]) {
-				return MultiPolygon{}, errors.New("polygon interiors must not intersect")
+	if doExpensiveValidations(opts) {
+		for i := 0; i < len(polys); i++ {
+			for j := i + 1; j < len(polys); j++ {
+				bound1 := polys[i].Boundary()
+				bound2 := polys[j].Boundary()
+				inter := bound1.Intersection(bound2)
+				if inter.Dimension() > 0 {
+					return MultiPolygon{}, errors.New("the boundaries of the polygon elements of multipolygons must only intersect at points")
+				}
+				if polyInteriorsIntersect(polys[i], polys[j]) {
+					return MultiPolygon{}, errors.New("polygon interiors must not intersect")
+				}
 			}
 		}
 	}
