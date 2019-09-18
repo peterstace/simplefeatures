9495c64ce0b078eb189c69795482646b40354375
Peter Stace <peterstace@gmail.com>
2019-09-19 06:49:57 +1000

    Revert "Move test bailout checks"
    This reverts commit 6e822f1d28a5128ab7ad6771d6cf7d5d5e51008e.

--- fuzztests/checks.go
+++ fuzztests/checks.go
@@ -350,17 +350,17 @@ func CheckEqualsExact(t *testing.T, pg PostGIS, g1, g2 geom.Geometry) {
 }
 
 func CheckEquals(t *testing.T, pg PostGIS, g1, g2 geom.Geometry) {
-	_, g1GC := g1.(geom.GeometryCollection)
-	_, g2GC := g2.(geom.GeometryCollection)
-	if g1GC || g2GC {
-		// PostGIS cannot calculate Equals for geometry collections.
-		return
-	}
-	got, err := g1.Equals(g2)
-	if err != nil {
-		return // operation not implemented
-	}
 	t.Run("CheckEquals", func(t *testing.T) {
+		_, g1GC := g1.(geom.GeometryCollection)
+		_, g2GC := g2.(geom.GeometryCollection)
+		if g1GC || g2GC {
+			// PostGIS cannot calculate Equals for geometry collections.
+			return
+		}
+		got, err := g1.Equals(g2)
+		if err != nil {
+			return // operation not implemented
+		}
 		want := pg.Equals(t, g1, g2)
 		if got != want {
 			t.Logf("got:  %t", got)
@@ -371,11 +371,11 @@ func CheckEquals(t *testing.T, pg PostGIS, g1, g2 geom.Geometry) {
 }
 
 func CheckIntersects(t *testing.T, pg PostGIS, g1, g2 geom.Geometry) {
-	got, err := g1.Intersects(g2)
-	if err != nil {
-		return // operation not implemented
-	}
 	t.Run("CheckIntersects", func(t *testing.T) {
+		got, err := g1.Intersects(g2)
+		if err != nil {
+			return // operation not implemented
+		}
 		want := pg.Intersects(t, g1, g2)
 		if got != want {
 			t.Logf("got:  %t", got)
