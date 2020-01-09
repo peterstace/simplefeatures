71c94c9fc46389366cf3b542e4bb817dc8378e61
Peter Stace <peterstace@gmail.com>
2020-01-09 19:34:20 +1100

    Check for nested rings in polygon validation

--- geom/validation_test.go
+++ geom/validation_test.go
@@ -103,6 +103,11 @@ func TestPolygonValidation(t *testing.T) {
 			(1 1,4 1,4 4,1 4,1 1),
 			(2 2,3 2,3 3,2 3,2 2)
 		)`,
+		`POLYGON(
+			(0 0,5 0,5 5,0 5,0 0),
+			(2 2,3 2,3 3,2 3,2 2),
+			(1 1,4 1,4 4,1 4,1 1)
+		)`,
 	} {
 		t.Run("invalid_"+strconv.Itoa(i), func(t *testing.T) {
 			_, err := UnmarshalWKT(strings.NewReader(wkt))
