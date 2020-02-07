27edd8a71b14100d2299db75c5748d0881733caf
Peter Stace <peterstace@gmail.com>
2020-02-07 14:37:54 +1100

    Add IsSimple

--- internal/libgeos/handle.go
+++ internal/libgeos/handle.go
@@ -2,6 +2,8 @@ package libgeos
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 
 	"github.com/peterstace/simplefeatures/geom"
 )
@@ -10,50 +12,31 @@ import (
 #cgo linux CFLAGS: -I/usr/include
 #cgo linux LDFLAGS: -L/usr/lib -lgeos_c
 #include "geos_c.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-void sf_notice_handler(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    fprintf(stderr, "NOTICE: ");
-    vfprintf(stderr, format, args);
-    va_end(args);
-}
-
-//XXX: error handler
-
-GEOSContextHandle_t sf_initGEOS() {
-	return initGEOS_r(sf_notice_handler, sf_notice_handler);
-}
-
 */
 import "C"
 
 type Handle struct {
-	h C.GEOSContextHandle_t
+	context C.GEOSContextHandle_t
 }
 
 func NewHandle() *Handle {
-	handle := &Handle{C.GEOS_init_r()}
-	//handle.h
-	// TODO: set notice and error handlers
+	return &Handle{C.GEOS_init_r()}
 }
 
-func (h Handle) Close() {
-	C.GEOS_finish_r(h.h)
+func (h *Handle) Close() {
+	C.GEOS_finish_r(h.context)
 }
 
-func (h Handle) createGeomHandle(g geom.Geometry) (*C.GEOSGeometry, func()) {
+func (h *Handle) createGeomHandle(g geom.Geometry) (*C.GEOSGeometry, func()) {
 	var wkb bytes.Buffer
 	if err := g.AsBinary(&wkb); err != nil {
 		panic(err) // can't fail writing to a buffer
 	}
 
-	reader := C.GEOSWKBReader_create_r(h.h)
-	defer C.GEOSWKBReader_destroy_r(h.h, reader)
+	reader := C.GEOSWKBReader_create_r(h.context)
+	defer C.GEOSWKBReader_destroy_r(h.context, reader)
 	gh := C.GEOSWKBReader_read_r(
-		h.h,
+		h.context,
 		reader,
 		(*C.uchar)(&wkb.Bytes()[0]),
 		C.ulong(wkb.Len()),
@@ -61,12 +44,31 @@ func (h Handle) createGeomHandle(g geom.Geometry) (*C.GEOSGeometry, func()) {
 	return gh, func() { C.GEOSGeom_destroy(gh) }
 }
 
-func (h Handle) AsText(g geom.Geometry) (string, error) {
+func (h *Handle) AsText(g geom.Geometry) (string, error) {
 	gh, destroy := h.createGeomHandle(g)
 	defer destroy()
 
-	writer := C.GEOSWKTWriter_create_r(h.h)
-	defer C.GEOSWKTWriter_destroy_r(h.h, writer)
-	wkt := C.GEOSWKTWriter_write_r(h.h, writer, gh)
+	writer := C.GEOSWKTWriter_create_r(h.context)
+	defer C.GEOSWKTWriter_destroy_r(h.context, writer)
+	wkt := C.GEOSWKTWriter_write_r(h.context, writer, gh)
 	return C.GoString(wkt), nil
+}
+
+func (h *Handle) IsSimple(g geom.Geometry) (bool, error) {
+	gh, destroy := h.createGeomHandle(g)
+	defer destroy()
+	return h.boolErr(C.GEOSisSimple_r(h.context, gh))
+}
+
+func (h *Handle) boolErr(c C.char) (bool, error) {
+	switch c {
+	case 0:
+		return false, nil
+	case 1:
+		return true, nil
+	case 2:
+		return false, errors.New("an exception occurred")
+	default:
+		return false, fmt.Errorf("illegal result from libgeos: %v", c)
+	}
 }
--- internal/libgeos/libgeos_test.go
+++ internal/libgeos/libgeos_test.go
@@ -24,3 +24,37 @@ func TestAsText(t *testing.T) {
 		t.Errorf("want: %v got: %v", want, wkt)
 	}
 }
+
+func TestIsSimpleValid(t *testing.T) {
+	g, err := geom.UnmarshalWKT(strings.NewReader("LINESTRING(0 0,0 1,1 0,0 0)"))
+	if err != nil {
+		t.Fatal("could not unmarshal WKT")
+	}
+
+	h := libgeos.NewHandle()
+	defer h.Close()
+	got, err := h.IsSimple(g)
+	if err != nil {
+		t.Fatal("could not get IsSimple")
+	}
+	if !got {
+		t.Errorf("expected it to be simple")
+	}
+}
+
+func TestIsNotSimpleInvalid(t *testing.T) {
+	g, err := geom.UnmarshalWKT(strings.NewReader("LINESTRING(0 0,1 1,1 0,0 1)"))
+	if err != nil {
+		t.Fatal("could not unmarshal WKT")
+	}
+
+	h := libgeos.NewHandle()
+	defer h.Close()
+	got, err := h.IsSimple(g)
+	if err != nil {
+		t.Fatal("could not get IsSimple")
+	}
+	if got {
+		t.Errorf("expected it not to be simple")
+	}
+}
