85db9222c5be320f837e882807c207697ae80221
Peter Stace <peterstace@gmail.com>
2020-02-07 14:46:48 +1100

    Reuse unmarshal buffer

--- internal/libgeos/handle.go
+++ internal/libgeos/handle.go
@@ -16,31 +16,37 @@ import (
 import "C"
 
 type Handle struct {
-	context C.GEOSContextHandle_t
+	context   C.GEOSContextHandle_t
+	wkbReader *C.GEOSWKBReader
+	wkbBuf    []byte
 }
 
 func NewHandle() *Handle {
-	return &Handle{C.GEOS_init_r()}
+	ctx := C.GEOS_init_r()
+	return &Handle{
+		context:   ctx,
+		wkbReader: C.GEOSWKBReader_create_r(ctx),
+	}
 }
 
 func (h *Handle) Close() {
+	C.GEOSWKBReader_destroy_r(h.context, h.wkbReader)
 	C.GEOS_finish_r(h.context)
 }
 
 func (h *Handle) createGeomHandle(g geom.Geometry) (*C.GEOSGeometry, func()) {
-	var wkb bytes.Buffer
-	if err := g.AsBinary(&wkb); err != nil {
+	wkb := bytes.NewBuffer(h.wkbBuf)
+	if err := g.AsBinary(wkb); err != nil {
 		panic(err) // can't fail writing to a buffer
 	}
-
-	reader := C.GEOSWKBReader_create_r(h.context)
-	defer C.GEOSWKBReader_destroy_r(h.context, reader)
+	h.wkbBuf = wkb.Bytes()
 	gh := C.GEOSWKBReader_read_r(
 		h.context,
-		reader,
-		(*C.uchar)(&wkb.Bytes()[0]),
+		h.wkbReader,
+		(*C.uchar)(&h.wkbBuf[0]),
 		C.ulong(wkb.Len()),
 	)
+	h.wkbBuf = h.wkbBuf[:0]
 	return gh, func() { C.GEOSGeom_destroy(gh) }
 }
 
@@ -57,10 +63,10 @@ func (h *Handle) AsText(g geom.Geometry) (string, error) {
 func (h *Handle) IsSimple(g geom.Geometry) (bool, error) {
 	gh, destroy := h.createGeomHandle(g)
 	defer destroy()
-	return h.boolErr(C.GEOSisSimple_r(h.context, gh))
+	return boolErr(C.GEOSisSimple_r(h.context, gh))
 }
 
-func (h *Handle) boolErr(c C.char) (bool, error) {
+func boolErr(c C.char) (bool, error) {
 	switch c {
 	case 0:
 		return false, nil
