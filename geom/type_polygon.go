9fcfa18c0bd7e936efd9fe6e49c38eafa9d91aee
Peter Stace <peterstace@gmail.com>
2020-02-28 14:41:35 +1100

    Fix typo in comment

--- geom/type_polygon.go
+++ geom/type_polygon.go
@@ -278,7 +278,7 @@ func (p Polygon) Intersects(g Geometry) bool {
 }
 
 func (p Polygon) IsEmpty() bool {
-	// Rings are now allowed to be empty, so we don't have to check IsEmpty on
+	// Rings are not allowed to be empty, so we don't have to check IsEmpty on
 	// each ring.
 	return len(p.rings) == 0
 }
