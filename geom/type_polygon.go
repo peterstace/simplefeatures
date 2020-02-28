95dd8d7ea8414912a93a7ef1daf427f92a1bb56f
Peter Stace <peterstace@gmail.com>
2020-02-28 13:55:53 +1100

    Update comment to clarify that rings cannot be empty.

--- geom/type_polygon.go
+++ geom/type_polygon.go
@@ -18,7 +18,7 @@ import (
 // Its assertions are:
 //
 // 1. The rings (outer and inner) must be valid linear rings (i.e. be simple
-// and closed LineStrings).
+// and closed LineStrings). This implies that the rings cannot be empty.
 //
 // 2. Each pair of rings must only intersect at a single point.
 //
