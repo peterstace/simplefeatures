0457567686e48c04ff7cbb3e2d782e88c82fef1d
Peter Stace <peterstace@gmail.com>
2020-03-07 10:47:59 +1100

    Use 2D geometries only for PostGIS intersection

--- fuzztests/postgis.go
+++ fuzztests/postgis.go
@@ -230,6 +230,10 @@ func (p PostGIS) Intersects(t *testing.T, g1, g2 geom.Geometry) bool {
 }
 
 func (p PostGIS) Intersection(t *testing.T, g1, g2 geom.Geometry) geom.Geometry {
+	// PostGIS returns an error if geometries with differing coordinate type
+	// are passed in (hence force to 2D).
+	g1 = g1.Force2D()
+	g2 = g2.Force2D()
 	return p.geomBinary(t, g1, g2, "ST_Intersection")
 }
 
