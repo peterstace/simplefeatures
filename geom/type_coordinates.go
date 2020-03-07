48a8262176d33cb69c6e826a789ad8c970a84912
Peter Stace <peterstace@gmail.com>
2020-03-08 08:22:18 +1100

    Remove unused functions

--- geom/type_coordinates.go
+++ geom/type_coordinates.go
@@ -5,38 +5,3 @@ type Coordinates struct {
 	Z float64
 	M float64
 }
-
-func oneDimXYToCoords(pts []XY) []Coordinates {
-	coords := make([]Coordinates, len(pts))
-	for i, pt := range pts {
-		coords[i] = Coordinates{XY: pt}
-	}
-	return coords
-}
-
-func twoDimXYToCoords(xys [][]XY) [][]Coordinates {
-	coords := make([][]Coordinates, len(xys))
-	for i, pts := range xys {
-		coords[i] = oneDimXYToCoords(pts)
-	}
-	return coords
-}
-
-func threeDimXYToCoords(xys [][][]XY) [][][]Coordinates {
-	coords := make([][][]Coordinates, len(xys))
-	for i, pts := range xys {
-		coords[i] = twoDimXYToCoords(pts)
-	}
-	return coords
-}
-
-// TODO: Add Z and M values
-// TODO: Want to have these methods removed.
-func (c Coordinates) MarshalJSON() ([]byte, error) {
-	buf := []byte{'['}
-	buf = appendFloat(buf, c.XY.X)
-	buf = append(buf, ',')
-	buf = appendFloat(buf, c.XY.Y)
-	buf = append(buf, ']')
-	return buf, nil
-}
