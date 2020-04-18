2f019897a93138f0d3f14798d528f4f58d526bf5
Peter Stace <peterstace@gmail.com>
2020-04-18 12:44:21 +1000

    Add benchmark script

---
+++ run_benchmarks.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ $# != 2 ]; then
+	echo "usage: $0 old_git_sh1 new_git_sha1"
+	exit 1
+fi
+
+old_git_sha1="$1"
+new_git_sha1="$2"
+
+old="$(mktemp)"
+new="$(mktemp)"
+trap "rm -f $new $old" EXIT
+
+for (( i = 0; i < 3; i++ )); do
+	echo
+	echo "RUN $i"
+
+	echo
+	echo "OLD"
+	git checkout "$old_git_sha1"
+	echo
+	go test ./... -test.run='^$' -bench=. | tee -a "$old"
+
+	echo
+	echo "NEW"
+	git checkout "$new_git_sha1"
+	echo
+	go test ./... -test.run='^$' -bench=. | tee -a "$new"
+done
+
+echo
+echo "OLD RESULTS"
+cat "$old"
+
+echo
+echo "NEW RESULTS"
+cat "$new"
+
+echo
+echo "COMPARISON"
+
+benchstat -alpha 0.15 "$old" "$new"
