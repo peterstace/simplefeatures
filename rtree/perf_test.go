6fe33a92a727c63aa3e9900f2f1940f8b329fbf4
Peter Stace <peterstace@gmail.com>
2020-05-24 08:38:41 +1000

    Use Stop/Start timer for setup

--- rtree/perf_test.go
+++ rtree/perf_test.go
@@ -9,16 +9,13 @@ import (
 func BenchmarkDelete(b *testing.B) {
 	for pop := 100; pop <= 10000; pop *= 10 {
 		b.Run(fmt.Sprintf("n=%d", pop), func(b *testing.B) {
-			var boxes []Box
-			rts := make([]RTree, b.N)
 			for i := 0; i < b.N; i++ {
+				b.StopTimer()
 				rnd := rand.New(rand.NewSource(0))
-				rts[i], boxes = testBulkLoad(rnd, pop, 0.9, 0.1)
-			}
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
+				rt, boxes := testBulkLoad(rnd, pop, 0.9, 0.1)
+				b.StartTimer()
 				for j, box := range boxes {
-					if !rts[i].Delete(box, j) {
+					if !rt.Delete(box, j) {
 						b.Fatal("could not delete")
 					}
 				}
