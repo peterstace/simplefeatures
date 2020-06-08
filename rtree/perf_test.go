f0147c69cf44ce0e8e0290881e2558cd94602d7e
Peter Stace <peterstace@gmail.com>
2020-06-09 05:59:19 +1000

    Add benchmark for bulk inserting

--- rtree/perf_test.go
+++ rtree/perf_test.go
@@ -23,3 +23,23 @@ func BenchmarkDelete(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkBulk(b *testing.B) {
+	for _, pop := range [...]int{10, 100, 1000} {
+		rnd := rand.New(rand.NewSource(0))
+		boxes := make([]Box, pop)
+		for i := range boxes {
+			boxes[i] = randomBox(rnd, 0.9, 0.1)
+		}
+		inserts := make([]BulkItem, len(boxes))
+		for i := range inserts {
+			inserts[i].Box = boxes[i]
+			inserts[i].RecordID = i
+		}
+		b.Run(fmt.Sprintf("n=%d", pop), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				BulkLoad(inserts)
+			}
+		})
+	}
+}
