78d917cf2d49da1075e25a9eb36cbbdaf680a2b9
Peter Stace <peterstace@gmail.com>
2020-10-16 16:36:00 +1100

    Alter behaviour for empty geometries

--- geom/alg_binary_op.go
+++ geom/alg_binary_op.go
@@ -3,24 +3,51 @@ package geom
 // Union returns a geometry that represents the parts from either geometry A or
 // geometry B (or both).
 func Union(a, b Geometry) Geometry {
+	if a.IsEmpty() && b.IsEmpty() {
+		return Geometry{}
+	}
+	if a.IsEmpty() {
+		return b
+	}
+	if b.IsEmpty() {
+		return a
+	}
 	return binaryOp(a, b, selectUnion)
 }
 
 // Intersection returns a geometry that represents the parts that are common to
 // both geometry A and geometry B.
 func Intersection(a, b Geometry) Geometry {
+	if a.IsEmpty() || b.IsEmpty() {
+		return Geometry{}
+	}
 	return binaryOp(a, b, selectIntersection)
 }
 
 // Difference returns a geometry that represents the parts of input geometry A
 // that are not part of input geometry B.
 func Difference(a, b Geometry) Geometry {
+	if a.IsEmpty() {
+		return Geometry{}
+	}
+	if b.IsEmpty() {
+		return a
+	}
 	return binaryOp(a, b, selectDifference)
 }
 
 // SymmetricDifference returns a geometry that represents the parts of geometry
 // A and B that are not in common.
 func SymmetricDifference(a, b Geometry) Geometry {
+	if a.IsEmpty() && b.IsEmpty() {
+		return Geometry{}
+	}
+	if a.IsEmpty() {
+		return b
+	}
+	if b.IsEmpty() {
+		return a
+	}
 	return binaryOp(a, b, selectSymmetricDifference)
 }
 
