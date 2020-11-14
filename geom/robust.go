9db066c574f36271c3b802d57fe114ddc6203819
Peter Stace <peterstace@gmail.com>
2020-11-14 19:20:44 +1100

    Implement exactMul

--- geom/robust.go
+++ geom/robust.go
@@ -15,3 +15,45 @@ func exponentFloat64(f float64) uint64 {
 	u := math.Float64bits(f)
 	return (u & 0x7ff0000000000000) >> 52
 }
+
+func exactMul(a, b float64) (p, q float64) {
+	a1 := clearLast27BitsOfMantissa(a)
+	b1 := clearLast27BitsOfMantissa(b)
+	a2 := a - a1
+	b2 := b - b1
+	p1 := a1 * b1
+	p2 := a1 * b2
+	p3 := a2 * b1
+	p4 := a2 * b2
+	var p5 float64
+	if bit27OfMantissaSet(a2) && bit27OfMantissaSet(b2) {
+		a3 := clear27thBitOfMantissa(a2)
+		a4 := a2 - a3
+		p4 = a3 * b2
+		p5 := a4 * b2
+		p4, p5 = exactSum(p4, p5)
+	}
+	p2, p3 = exactSum(p2, p3)
+	p3, p4 = exactSum(p3, p4)
+	p2, p3 = exactSum(p2, p3)
+	p, p2 = exactSum(p1, p2)
+	q = (p2 + p3) + (p4 + p5)
+	return
+}
+
+func clearLast27BitsOfMantissa(f float64) float64 {
+	u := math.Float64bits(f)
+	u = u &^ 0x7ffffff
+	return math.Float64frombits(u)
+}
+
+func clear27thBitOfMantissa(f float64) float64 {
+	u := math.Float64bits(f)
+	u = u &^ 0x2000000
+	return math.Float64frombits(u)
+}
+
+func bit27OfMantissaSet(f float64) bool {
+	u := math.Float64bits(f)
+	return (u & 0x2000000) != 0
+}
--- geom/robust_test.go
+++ geom/robust_test.go
@@ -40,3 +40,44 @@ func TestExactSum(t *testing.T) {
 		})
 	}
 }
+
+func TestExactMul(t *testing.T) {
+	for i, tt := range []struct {
+		a, b float64
+	}{
+		{0, 0},
+		{1, 1},
+		{0.1, 0.1},
+		{1e0, 1e10},
+		{1e0, 1e20},
+		{1e0, 1e40},
+		{1e-20, 1e20},
+		{-200, 200},
+		{math.Pi, math.E},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			gotP, gotQ := exactMul(tt.a, tt.b)
+			gotMul := new(big.Rat).Add(
+				new(big.Rat).SetFloat64(gotP),
+				new(big.Rat).SetFloat64(gotQ),
+			)
+			wantMul := new(big.Rat).Mul(
+				new(big.Rat).SetFloat64(tt.a),
+				new(big.Rat).SetFloat64(tt.b),
+			)
+			t.Logf("a: %v", tt.a)
+			t.Logf("b: %v", tt.b)
+			t.Logf("p: %v", gotP)
+			t.Logf("q: %v", gotQ)
+			if gotMul.Cmp(wantMul) != 0 {
+				t.Errorf("got: %v, want: %v", gotMul, wantMul)
+			}
+		})
+	}
+}
+
+func BenchmarkExactMul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		exactMul(math.Pi, math.E)
+	}
+}
