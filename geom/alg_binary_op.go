f0a075b3fb95d6aaf5578c9d15a7eae8d961e643
Peter Stace <peterstace@gmail.com>
2020-12-30 10:13:03 +1100

    Reduce calls to nodeSet.insertOrGet

--- geom/alg_binary_op.go
+++ geom/alg_binary_op.go
@@ -25,16 +25,6 @@ func Union(a, b Geometry) (Geometry, error) {
 // both geometry A and geometry B. An error may be returned in pathological
 // cases of numerical degeneracy. GeometryCollections are not supported.
 func Intersection(a, b Geometry) (Geometry, error) {
-	insertOrGetCount = 0
-	appendNewNodeCount = 0
-	appendLinesInsertOrGetCount = 0
-	reNodeLineStringInsertOrGetCount = 0
-	defer func() {
-		fmt.Println("insertOrGetCount", insertOrGetCount)
-		fmt.Println("appendNewNodeCount", appendNewNodeCount)
-		fmt.Println("appendLinesInsertOrGetCount", appendLinesInsertOrGetCount)
-		fmt.Println("reNodeLineStringInsertOrGetCount", reNodeLineStringInsertOrGetCount)
-	}()
 	if a.IsEmpty() || b.IsEmpty() {
 		return Geometry{}, nil
 	}
