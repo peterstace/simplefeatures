8720126ee2afde290ff3f59abaf76a27cb991282
Peter Stace <peterstace@gmail.com>
2021-06-07 05:46:23 +1000

    Fix bug in error wrapping

--- geos/libgeos.go
+++ geos/libgeos.go
@@ -632,5 +632,5 @@ func wrap(err error, format string, args ...interface{}) error {
 	if err == nil {
 		return nil
 	}
-	return fmt.Errorf(format+": %v", append(args, err))
+	return fmt.Errorf(format+": %v", append(args, err)...)
 }
