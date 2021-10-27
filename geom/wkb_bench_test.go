156a23108ad5775f1bdab3f35a49240e8034fa5a
missinglink <insomnia@rcpt.at>
2021-10-27 11:01:57 +0200

    wkb: add benchmarking script

---
+++ geom/wkb_bench_test.go
@@ -0,0 +1,21 @@
+package geom_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/peterstace/simplefeatures/geom"
+)
+
+func BenchmarkWKBParse(b *testing.B) {
+	for i, tt := range validWKBCases {
+		b.Run(strconv.Itoa(i), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				_, err := geom.UnmarshalWKB(hexStringToBytes(b, tt.wkb), geom.DisableAllValidations)
+				if err != nil {
+					panic(err)
+				}
+			}
+		})
+	}
+}
