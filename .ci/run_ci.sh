25b695b481f168a3219cc4d75bd69fe8197e1e6d
Peter Stace <peterstace@gmail.com>
2022-11-05 10:24:00 +1100

    Disable cmpgeos tests

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -10,6 +10,6 @@ docker-compose -f "$here/docker-compose-unit.yml"    up --abort-on-container-exi
 docker-compose -f "$here/docker-compose-geos.yml"    up --abort-on-container-exit
 docker-compose -f "$here/docker-compose-pgscan.yml"  up --abort-on-container-exit
 docker-compose -f "$here/docker-compose-cmppg.yml"   up --abort-on-container-exit
-docker-compose -f "$here/docker-compose-cmpgeos.yml" up --abort-on-container-exit
+#docker-compose -f "$here/docker-compose-cmpgeos.yml" up --abort-on-container-exit
 
 printf "\nduration: %.1f seconds\n" "$(echo "$(date +%s.%N) - $start" | bc)"
