8c4be898e8b75dd13efb70c84a5a084c5d86d3e3
Peter Stace <peterstace@gmail.com>
2023-02-11 08:41:33 +1100

    Add tests for inner ring orientation

--- geom/normalize_test.go
+++ geom/normalize_test.go
@@ -43,6 +43,16 @@ func TestNormalize(t *testing.T) {
 		{"POLYGON((0 0,0 1,1 0,0 0))", "POLYGON((0 0,1 0,0 1,0 0))"},
 		{"POLYGON((0 0,1 0,0 1,0 0))", "POLYGON((0 0,1 0,0 1,0 0))"},
 
+		// Normalises inner ring orientations:
+		{
+			"POLYGON((0 0,3 0,3 3,0 3,0 0),(1 1,1 2,2 2,2 1,1 1))",
+			"POLYGON((0 0,3 0,3 3,0 3,0 0),(1 1,1 2,2 2,2 1,1 1))",
+		},
+		{
+			"POLYGON((0 0,3 0,3 3,0 3,0 0),(1 1,2 1,2 2,1 2,1 1))",
+			"POLYGON((0 0,3 0,3 3,0 3,0 0),(1 1,1 2,2 2,2 1,1 1))",
+		},
+
 		// Normalises ring starting points:
 		{"POLYGON((0 0,1 0,0 1,0 0))", "POLYGON((0 0,1 0,0 1,0 0))"},
 		{"POLYGON((1 0,0 1,0 0,1 0))", "POLYGON((0 0,1 0,0 1,0 0))"},
