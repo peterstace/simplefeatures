ae47d78c50c12e6353854c6752aa81de183aa389
Peter Stace <peterstace@gmail.com>
2023-03-21 09:05:08 +1100

    Remove PostGIS GeoJSON reference implementation tests
    These are not worth the hassle that they're bringing.

--- internal/cmprefimpl/cmppg/fuzz_test.go
+++ internal/cmprefimpl/cmppg/fuzz_test.go
@@ -23,7 +23,6 @@ func TestFuzz(t *testing.T) {
 
 	checkWKTParse(t, pg, candidates)
 	checkWKBParse(t, pg, candidates)
-	checkGeoJSONParse(t, pg, candidates)
 
 	geoms := convertToGeometries(t, candidates)
 
