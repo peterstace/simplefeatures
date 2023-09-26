9073630afa16e50b174b47808332d7aa546f24e9
Peter Stace <peterstace@gmail.com>
2023-09-27 05:14:18 +1000

    Fix typo in doc comment of NoValidate

--- geom/no_validate.go
+++ geom/no_validate.go
@@ -1,7 +1,7 @@
 package geom
 
 // NoValidate causes functions to skip geometry constraint validation.
-// Functions where validation can be skipped accepted as a variadic list of
+// Functions where validation can be skipped accept a variadic list of
 // NoValidate values. If at least one NoValidate value is passed in, then the
 // function will skip validation, otherwise it will perform validation as its
 // default behaviour.
