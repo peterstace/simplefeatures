29c694dc493870b1de1e266cd2e0b4b6b82fdaaf
Peter Stace <peterstace@gmail.com>
2023-11-22 15:02:16 +1100

    Simpler `nodeSet` implementation

--- geom/dcel_node_set.go
+++ geom/dcel_node_set.go
@@ -4,19 +4,6 @@ import (
 	"math"
 )
 
-func newNodeSet(maxULPSize float64, sizeHint int) nodeSet {
-	// The appropriate multiplication factor to use to calculate bucket size is
-	// a bit of a guess.
-	bucketSize := maxULPSize * 0x200
-	return nodeSet{
-		bucketSize,
-		make(map[nodeBucket]XY, sizeHint),
-	}
-}
-
-// nodeSet is a set of XY values (nodes). If an XY value is inserted, but it is
-// "close" to an existing XY in the set, then the original XY is returned (and
-// the new XY _not_ inserted). The two XYs essentially merge together.
 type nodeSet struct {
 	bucketWidth float64
 	nodes       map[nodeBucket]XY
@@ -26,25 +13,21 @@ type nodeBucket struct {
 	x, y int
 }
 
-func (s nodeSet) insertOrGet(xy XY) XY {
-	b := nodeBucket{
-		int(math.Floor(xy.X / s.bucketWidth)),
-		int(math.Floor(xy.Y / s.bucketWidth)),
+func newNodeSet(ulp float64, sizeHint int) nodeSet {
+	return nodeSet{
+		ulp * 0x1000,
+		make(map[nodeBucket]XY, sizeHint),
 	}
-	for _, offset := range [...]nodeBucket{
-		b,
-		{b.x - 1, b.y - 1},
-		{b.x - 1, b.y},
-		{b.x - 1, b.y + 1},
-		{b.x, b.y - 1},
-		{b.x, b.y + 1},
-		{b.x + 1, b.y - 1},
-		{b.x + 1, b.y},
-		{b.x + 1, b.y + 1},
-	} {
-		if node, ok := s.nodes[offset]; ok {
-			return node
-		}
+}
+
+func (s nodeSet) insertOrGet(xy XY) XY {
+	half := 0.5 * s.bucketWidth
+	b := nodeBucket{
+		x: int(math.Floor((xy.X + half) / s.bucketWidth)),
+		y: int(math.Floor((xy.Y + half) / s.bucketWidth)),
+	}
+	if node, ok := s.nodes[b]; ok {
+		return node
 	}
 	s.nodes[b] = xy
 	return xy
