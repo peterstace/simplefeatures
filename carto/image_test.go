f094fb2549f5e0265bf18fd2e7f834e9316a128c
Peter Stace <peterstace@gmail.com>
2024-10-11 17:19:37 +1100

    Get orthographic projection tests working

--- carto/image_test.go
+++ carto/image_test.go
@@ -105,9 +105,72 @@ func TestWorldProjections(t *testing.T) {
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			path := filepath.Join("./testdata", fmt.Sprintf("%d_%s", i, tc.filename))
-			writeProjectedWorld(t, path, tc.proj, tc.pxWide, tc.pxHigh, tc.tlXY, tc.brXY, tc.mask)
+			worldMask := geom.NewSingleRingPolygonXY(-180, 90, 180, 90, 180, -90, -180, -90, -180, 90)
+			writeProjectedWorld(t, path, tc.proj, tc.pxWide, tc.pxHigh, tc.tlXY, tc.brXY, tc.mask, worldMask)
 		})
 	}
+
+	t.Run("orthographic from south pole", func(t *testing.T) {
+		worldMask := geom.NewSingleRingPolygonXY(-180, 0, 180, 0, 180, -90, -180, -90, -180, 0)
+		path := filepath.Join("./testdata", "orthographic_south_pole.png")
+		writeProjectedWorld(
+			t,
+			path,
+			carto.NewOrthographic(earthRadius, geom.XY{X: 135, Y: -90}).To,
+			512, 512,
+			geom.XY{X: -earthRadius, Y: +earthRadius},
+			geom.XY{X: +earthRadius, Y: -earthRadius},
+			circle(geom.XY{X: 256, Y: 256}, 256),
+			worldMask,
+		)
+	})
+
+	t.Run("orthographic from north pole", func(t *testing.T) {
+		worldMask := geom.NewSingleRingPolygonXY(-180, 0, 180, 0, 180, 90, -180, 90, -180, 0)
+		path := filepath.Join("./testdata", "orthographic_north_pole.png")
+		writeProjectedWorld(
+			t,
+			path,
+			carto.NewOrthographic(earthRadius, geom.XY{X: 15, Y: 90}).To,
+			512, 512,
+			geom.XY{X: -earthRadius, Y: +earthRadius},
+			geom.XY{X: +earthRadius, Y: -earthRadius},
+			circle(geom.XY{X: 256, Y: 256}, 256),
+			worldMask,
+		)
+	})
+
+	t.Run("orthographic from north america", func(t *testing.T) {
+		const centralMeridian = -105
+		var worldMaskCoords []float64
+		latAtLon := func(lon float64) float64 {
+			return 45 * math.Cos((285+lon)*math.Pi/180)
+		}
+		for lon := -180.0; lon <= 180.0; lon++ {
+			lat := latAtLon(lon)
+			worldMaskCoords = append(worldMaskCoords, lon, lat)
+		}
+		worldMaskCoords = append(
+			worldMaskCoords,
+			180, latAtLon(180),
+			180, 90,
+			-180, 90,
+			-180, latAtLon(-180),
+		)
+		worldMask := geom.NewPolygonXY(worldMaskCoords)
+
+		path := filepath.Join("./testdata", "orthographic_north_america.png")
+		writeProjectedWorld(
+			t,
+			path,
+			carto.NewOrthographic(earthRadius, geom.XY{X: centralMeridian, Y: 45}).To,
+			512, 512,
+			geom.XY{X: -earthRadius, Y: +earthRadius},
+			geom.XY{X: +earthRadius, Y: -earthRadius},
+			circle(geom.XY{X: 256, Y: 256}, 256),
+			worldMask,
+		)
+	})
 }
 
 func writeProjectedWorld(
@@ -118,7 +181,8 @@ func writeProjectedWorld(
 	pxHigh int,
 	tlXY geom.XY,
 	brXY geom.XY,
-	maskPoly geom.Polygon,
+	mapMask geom.Polygon,
+	worldMask geom.Polygon,
 ) {
 	var (
 		waterColor = color.RGBA{R: 144, G: 218, B: 238, A: 255}
@@ -131,6 +195,13 @@ func writeProjectedWorld(
 	glaciers := loadGeom(t, "testdata/ne_50m_glaciated_areas.geojson.gz")
 	iceshelves := loadGeom(t, "testdata/ne_50m_antarctic_ice_shelves_polys.geojson.gz")
 
+	worldMask = worldMask.Densify(1)
+	for _, g := range []*geom.Geometry{&land, &lakes, &glaciers, &iceshelves} {
+		clipped, err := geom.Intersection(*g, worldMask.AsGeometry())
+		*g = clipped
+		expectNoErr(t, err)
+	}
+
 	var lines []geom.LineString
 	for lon := -180; lon <= 180; lon += 30 {
 		line := geom.NewLineString(geom.NewSequence([]float64{float64(lon), -90, float64(lon), +90}, geom.DimXY))
@@ -142,6 +213,14 @@ func writeProjectedWorld(
 		line = line.Densify(1)
 		lines = append(lines, line)
 	}
+
+	var clippedLines []geom.LineString
+	for i := range lines {
+		clipped, err := geom.Intersection(lines[i].AsGeometry(), worldMask.AsGeometry())
+		clippedLines = append(clippedLines, extractLinearParts(clipped)...)
+		expectNoErr(t, err)
+	}
+	lines = clippedLines
 
 	for _, g := range []*geom.Geometry{&land, &lakes, &glaciers, &iceshelves} {
 		*g = g.TransformXY(func(latlon geom.XY) geom.XY {
@@ -168,36 +247,46 @@ func writeProjectedWorld(
 				Y: linearRemap(tlXY.Y, 0, brXY.Y, float64(pxHigh))(xy.Y),
 			}
 		})
-
 	}
 
 	rast := rasterize.NewRasterizer(pxWide, pxHigh)
 
-	// TODO: Programmatic mask instead of geometry based mask.
-	mask := image.NewAlpha(image.Rect(0, 0, pxWide, pxHigh))
-	if maskPoly.IsEmpty() {
-		draw.Draw(mask, mask.Bounds(), image.NewUniform(color.Opaque), image.Point{}, draw.Src)
+	mapMaskImage := image.NewAlpha(image.Rect(0, 0, pxWide, pxHigh))
+	var mapOutline geom.MultiLineString
+	if mapMask.IsEmpty() {
+		draw.Draw(mapMaskImage, mapMaskImage.Bounds(), image.NewUniform(color.Opaque), image.Point{}, draw.Src)
 	} else {
-		mask = image.NewAlpha(image.Rect(0, 0, pxWide, pxHigh))
+		mapMaskImage = image.NewAlpha(image.Rect(0, 0, pxWide, pxHigh))
 		rast.Reset()
-		rast.Polygon(maskPoly)
-		rast.Draw(mask, mask.Bounds(), image.NewUniform(color.Opaque), image.Point{})
+		rast.Polygon(mapMask)
+		rast.Draw(mapMaskImage, mapMaskImage.Bounds(), image.NewUniform(color.Opaque), image.Point{})
+		mapOutline = mapMask.Boundary()
 	}
 
 	img := image.NewRGBA(image.Rect(0, 0, pxWide, pxHigh))
-	draw.DrawMask(img, img.Bounds(), image.NewUniform(waterColor), image.Point{}, mask, image.Point{}, draw.Src)
+	draw.DrawMask(img, img.Bounds(), image.NewUniform(waterColor), image.Point{}, mapMaskImage, image.Point{}, draw.Src)
 
 	rast.Reset()
-	rast.MultiPolygon(land.MustAsMultiPolygon())
+	rast.MultiLineString(mapOutline)
+	rast.Draw(img, img.Bounds(), image.NewUniform(color.Black), image.Point{})
+
+	rasterisePolygons := func(g geom.Geometry) {
+		for _, p := range extractPolygonalParts(g) {
+			rast.Polygon(p)
+		}
+	}
+
+	rast.Reset()
+	rasterisePolygons(land)
 	rast.Draw(img, img.Bounds(), image.NewUniform(landColor), image.Point{})
 
 	rast.Reset()
-	rast.MultiPolygon(lakes.MustAsMultiPolygon())
+	rasterisePolygons(lakes)
 	rast.Draw(img, img.Bounds(), image.NewUniform(waterColor), image.Point{})
 
 	rast.Reset()
-	rast.MultiPolygon(glaciers.MustAsMultiPolygon())
-	rast.MultiPolygon(iceshelves.MustAsMultiPolygon())
+	rasterisePolygons(glaciers)
+	rasterisePolygons(iceshelves)
 	rast.Draw(img, img.Bounds(), image.NewUniform(iceColor), image.Point{})
 
 	rast.Reset()
@@ -261,4 +350,49 @@ func linearRemap(fromA, toA, fromB, toB float64) func(float64) float64 {
 		t := (f - fromA) / fromDelta
 		return toA + t*toDelta
 	}
+}
+
+func extractPolygonalParts(g geom.Geometry) []geom.Polygon {
+	switch gt := g.Type(); gt {
+	case geom.TypeGeometryCollection:
+		var ps []geom.Polygon
+		for _, g := range g.Dump() {
+			ps = append(ps, extractPolygonalParts(g)...)
+		}
+		return ps
+	case geom.TypeMultiPolygon:
+		return g.MustAsMultiPolygon().Dump()
+	case geom.TypePolygon:
+		return []geom.Polygon{g.MustAsPolygon()}
+	default:
+		return nil
+	}
+}
+
+func extractLinearParts(g geom.Geometry) []geom.LineString {
+	switch gt := g.Type(); gt {
+	case geom.TypeGeometryCollection:
+		var lss []geom.LineString
+		for _, g := range g.Dump() {
+			lss = append(lss, extractLinearParts(g)...)
+		}
+		return lss
+	case geom.TypeMultiLineString:
+		return g.MustAsMultiLineString().Dump()
+	case geom.TypeLineString:
+		return []geom.LineString{g.MustAsLineString()}
+	default:
+		return nil
+	}
+}
+
+func circle(center geom.XY, radius float64) geom.Polygon {
+	var coords []float64
+	for i := 0.0; i <= 360; i++ {
+		coords = append(coords,
+			center.X+radius*math.Cos(i*math.Pi/180),
+			center.Y+radius*math.Sin(i*math.Pi/180),
+		)
+	}
+	return geom.NewPolygonXY(coords)
 }
