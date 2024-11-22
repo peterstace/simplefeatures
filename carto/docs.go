289a9dea50919268aaf8cb85666c3d7bfbb5b6a5
Peter Stace <peterstace@gmail.com>
2024-11-22 13:00:17 +1100

    Add package doc

---
+++ carto/docs.go
@@ -0,0 +1,10 @@
+// Package carto provides cartography functionality for working with and making
+// maps.
+//
+// This includes:
+//
+//   - Various projections between angular coordinates (longitude and latitude)
+//     and planar coordinates (x and y).
+//
+//   - Earth radius definitions.
+package carto
--- carto/radius.go
+++ carto/radius.go
@@ -1,7 +1,13 @@
 package carto
 
 const (
+	// WGS84EllipsoidEquatorialRadiusM is the radius of the WGS84 ellipsoid at
+	// the equator.
 	WGS84EllipsoidEquatorialRadiusM = 6378137.0
-	WGS84EllipsoidPolarRadiusM      = 6356752.314245
-	WGS84EllipsoidMeanRadiusM       = (2*WGS84EllipsoidEquatorialRadiusM + WGS84EllipsoidPolarRadiusM) / 3
+
+	// WGS84EllipsoidPolarRadiusM is the radius of the WGS84 ellipsoid at the poles.
+	WGS84EllipsoidPolarRadiusM = 6356752.314245
+
+	// WGS84EllipsoidMeanRadiusM is the mean radius of the WGS84 ellipsoid.
+	WGS84EllipsoidMeanRadiusM = (2*WGS84EllipsoidEquatorialRadiusM + WGS84EllipsoidPolarRadiusM) / 3
 )
