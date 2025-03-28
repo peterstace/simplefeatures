dbfbcabfcea5c59e03558c40c777a87430de3ffb
Peter Stace <peterstace@gmail.com>
2025-03-28 12:37:57 +1100

    Add initial proj wrapper

---
+++ proj/proj.go
@@ -0,0 +1,93 @@
+package proj
+
+/*
+#cgo LDFLAGS: -lproj
+#include <proj.h>
+*/
+import "C"
+import (
+	"fmt"
+
+	"github.com/peterstace/simplefeatures/geom"
+)
+
+type PJ struct {
+	ctx *C.PJ_CONTEXT
+	pj  *C.PJ
+}
+
+func NewPJ(sourceCRS, targetCRS string) (*PJ, error) {
+	c := C.proj_context_create()
+
+	p := C.proj_create_crs_to_crs(c, C.CString(sourceCRS), C.CString(targetCRS), nil)
+	if p == nil {
+		errno := C.proj_context_errno(c)
+		errstr := C.GoString(C.proj_context_errno_string(c, errno))
+		return nil, fmt.Errorf("proj_create failed: %s", errstr)
+	}
+	defer C.proj_destroy(p)
+
+	n := C.proj_normalize_for_visualization(c, p)
+	if n == nil {
+		errno := C.proj_context_errno(c)
+		errstr := C.GoString(C.proj_context_errno_string(c, errno))
+		return nil, fmt.Errorf("proj_normalize_for_visualization failed: %s", errstr)
+	}
+	return &PJ{c, n}, nil
+}
+
+func (p *PJ) Forward(ct geom.CoordinatesType, coords []float64) error {
+	return p.transform(C.PJ_FWD, ct, coords)
+}
+
+func (p *PJ) Inverse(ct geom.CoordinatesType, coords []float64) error {
+	return p.transform(C.PJ_INV, ct, coords)
+}
+
+func (p *PJ) transform(dir C.PJ_DIRECTION, ct geom.CoordinatesType, coords []float64) error {
+	if len(coords)%int(ct.Dimension()) != 0 {
+		return fmt.Errorf("len(coords) must be a multiple of ct.Dimension()")
+	}
+
+	var (
+		stride = C.size_t(ct.Dimension())
+		count  = C.size_t(len(coords) / int(ct.Dimension()))
+	)
+
+	p0 := (*C.double)(&coords[0]) // Always X.
+	p1 := (*C.double)(&coords[1]) // Always Y.
+
+	var p2 *C.double
+	if ct.Dimension() >= 3 {
+		p2 = (*C.double)(&coords[2]) // Z (if has Z), otherwise M.
+	}
+
+	var p3 *C.double
+	if ct.Dimension() == 4 {
+		p3 = (*C.double)(&coords[3]) // Always M (must have Z as well).
+	}
+
+	C.proj_trans_generic(
+		p.pj, dir,
+		p0, stride, count,
+		p1, stride, count,
+		p2, stride, count,
+		p3, stride, count,
+	)
+	errno := C.proj_errno(p.pj)
+	if errno != 0 {
+		C.proj_errno_reset(p.pj)
+		errstr := C.GoString(C.proj_context_errno_string(p.ctx, errno))
+		return fmt.Errorf("proj_trans_generic failed: %s", errstr)
+	}
+	return nil
+}
+
+func (p *PJ) Release() {
+	if p.pj != nil {
+		C.proj_destroy(p.pj)
+	}
+	if p.ctx != nil {
+		C.proj_context_destroy(p.ctx)
+	}
+}
---
+++ proj/proj_test.go
@@ -0,0 +1,29 @@
+package proj_test
+
+import (
+	"testing"
+
+	"github.com/peterstace/simplefeatures/geom"
+	"github.com/peterstace/simplefeatures/proj"
+)
+
+func TestBasic(t *testing.T) {
+	pj, err := proj.NewPJ("EPSG:4326", "+proj=utm +zone=32 +datum=WGS84")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pj.Release()
+
+	coords := []float64{55, 12} // lon, lat
+	t.Logf("coords: %v", coords)
+
+	if err := pj.Forward(geom.DimXY, coords); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("coords: %v", coords)
+
+	if err := pj.Inverse(geom.DimXY, coords); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("coords: %v", coords)
+}
