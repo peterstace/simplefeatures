c9fc0236c279ef69e220365d25fad5e80441ef64
Peter Stace <peterstace@gmail.com>
2026-01-20 20:25:31 +1100

    Clean up doc comment

--- internal/jtsport/java/doc.go
+++ internal/jtsport/java/doc.go
@@ -1,5 +1,4 @@
 // Package java provides utilities for transliterating Java code to Go. It
 // includes functions that emulate Java-specific behaviour where Go differs,
-// and helpers for implementing Java's class-based polymorphism using Go's
-// child-chain dispatch pattern.
+// and helpers for implementing Java's class-based polymorphism.
 package java
